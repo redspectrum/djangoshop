fd814e9e30321f666d6ffe657ba02c39f9fafb24
sergey.volodzko <sergey.volodzko@1000geeks.com>
2019-03-30 19:00:52 +0300

    4

--- pizzashopproject/wsgi.py
+++ pizzashopproject/wsgi.py
@@ -7,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-# import os
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzashopproject.settings')
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzashopproject.settings')
+
 from django.core.wsgi import get_wsgi_application
-
-
 
 
 application = get_wsgi_application()
